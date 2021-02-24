598da8ea936151aa52a0f0df32c7ea929df92974
Bryan Sullivan <Bsully505@yahoo.com>
2021-02-23 19:17:35 -0500

    added intro player names

--- src/BranchCSC109.java
+++ src/BranchCSC109.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class BranchCSC109 {
     String[][] fourBoard;
+    String Player1;
+    String Player2;
+    Scanner input ;
     public BranchCSC109(){
         fourBoard = new String[7][6];
+        input =  new Scanner(System.in);
+        run();
     }
     public static void main(String[] args){
         BranchCSC109 Connectfour = new BranchCSC109();
 
     }
     public void run(){
+        System.out.println("please enter player 1's name");
+        Player1 = input.nextLine();
+        System.out.println("please enter player 2's name");
+        Player2 = input.nextLine();
+        System.out.println("Welcome to my connect four game "+Player1+" and "+ Player2);
+
 
     }
 
