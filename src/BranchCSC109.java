898a824a78901f2a60dab48f60eb98cd6a592bdf
Bryan Sullivan <Bsully505@yahoo.com>
2021-02-24 12:28:25 -0500

    added an input checker to check if they chose a viable column

--- src/BranchCSC109.java
+++ src/BranchCSC109.java
@@ -3,6 +3,7 @@
  * Date: Tuesday Feb 23
  *
  * Still need to implement: diagonal winning
+ * Possible ideas to add on: playing again after someone has won or a stalemate, a quit command
  */
 
 import java.util.Scanner;
@@ -20,7 +21,6 @@ public class BranchCSC109 {
         input =  new Scanner(System.in);
         win = false;
         MoveNum = 0;
-
         run();
     }
     public static void main(String[] args){
@@ -28,24 +28,36 @@ public class BranchCSC109 {
 
     }
     public void run(){
-//        System.out.println("please enter player 1's name");
-//        Player1 = input.nextLine();
-//        System.out.println("please enter player 2's name");
-//        Player2 = input.nextLine();
-//        System.out.println("Welcome to my connect four game "+Player1+" and "+ Player2);
-//        //im going to have to incorporate populating the
-//        System.out.println("Rules: \n1)Player one is x and goes first \n" +
-//                "2)First player with a continuation of 4 wins \n3)When playing your turn you can only go in a not full column of 1 through 7");
+        System.out.println("please enter player 1's name");
+        Player1 = input.nextLine();
+        System.out.println("please enter player 2's name");
+        Player2 = input.nextLine();
+        System.out.println("Welcome to my connect four game "+Player1+" and "+ Player2);
+        System.out.println("Rules: \n1)Player one is x and goes first \n" +
+                "2)First player with a continuation of 4 wins \n3)When playing your turn you can only go in a not full column of 1 through 7");
         boolean turner = true;
         while(!win && MoveNum < 42){//need to double check that it is 42 which would allow for the whole board to be filled
 
             System.out.println("Please enter which column you are dropping your tocken in");
+            boolean flagger = true;
+            int Column =-1;
+            while(flagger) {
+                String Col = input.nextLine();
+                try {
+                    Column = Integer.parseInt(Col) - 1;
+                    if(Column >=0 && Column <7){
+                        flagger = false;
+                    }
+                    else{
+                        System.out.println("please enter a number 1 through 7 ex. 3");
+                    }
+                }
+                catch (NumberFormatException e){
+                    System.out.println("You did not enter a number, please enter a number 1 through 7 ex. 3");
+                }
 
+            }
 
-            String Col = input.nextLine();
-            //add a parse checker if statement where the number how to be 1-7 and an integer
-
-            int Column  = Integer.parseInt(Col)-1;
             if(fourBoard[0][Column]== null){
                 int zed = 0;
                 boolean pass = true;
@@ -81,6 +93,9 @@ public class BranchCSC109 {
             } else {
                 System.out.println("the winner is " + Player2);
             }
+        }
+        else{
+            System.out.println("it seems like no one has won :(");
         }
         //also if the whole board is played i will have to say that no one has won.
 
@@ -137,9 +152,6 @@ public class BranchCSC109 {
         if(coller.contains(key)){
             return true;
         }
-
-
-
 
         return false;
     }
